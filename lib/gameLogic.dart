0865bdc4bb7d01387e1487266c5c78683a946eaf
goranesine <42481446+goranesine@users.noreply.github.com>
2022-12-15 20:10:54 +0100

    start_working_on_game_page

--- lib/gameLogic.dart
+++ lib/gameLogic.dart
@@ -51,8 +51,8 @@ class GameLogic extends ChangeNotifier {
   bool playerTwoWon = false;
 
   GameLogic() {
-  //  colorsPlayerOne.shuffle();
-  //  colorsPlayerTwo.shuffle();
+    //  colorsPlayerOne.shuffle();
+    //  colorsPlayerTwo.shuffle();
   }
 
   void startNewGame() {
@@ -105,112 +105,108 @@ class GameLogic extends ChangeNotifier {
 
   void gameLoop(int player, int blockIndex) {
     handleUserInput(player, blockIndex);
-    player == 1
-        ? Future.delayed(
-            const Duration(seconds: 1), () => updateStatesPlayerOne())
-        : Future.delayed(
-            const Duration(seconds: 1), () => updateStatesPlayerTwo());
+    Future.delayed(const Duration(seconds: 1), () => updateStates(player));
   }
 
   void handleUserInput(int player, int blockIndex) {
-    player == 1 ? playerOneTap(blockIndex) : playerTwoTap(blockIndex);
-  }
-
-  void playerOneTap(int blockIndex) {
-    if (playerOneIsTapped[blockIndex] == false &&
-        playerOneTappedColors.length < 4) {
-      playerOneTappedColors[blockIndex] = colorsPlayerOne[blockIndex];
-      playerOneIsTapped[blockIndex] = true;
-      notifyListeners();
-    }
-  }
-
-  void playerTwoTap(int blockIndex) {
-    if (playerTwoIsTapped[blockIndex] == false &&
-        playerTwoTappedColors.length < 4) {
-      playerTwoTappedColors[blockIndex] = colorsPlayerTwo[blockIndex];
-      playerTwoIsTapped[blockIndex] = true;
-      notifyListeners();
-    }
-  }
-
-  void updateStatesPlayerOne() {
-    if (playerOneTappedColors.length == 4) {
-      if (playerOneTappedColors.values.elementAt(0) ==
-              playerOneTappedColors.values.elementAt(1) &&
-          playerOneTappedColors.values.elementAt(0) ==
-              playerOneTappedColors.values.elementAt(2) &&
-          playerOneTappedColors.values.elementAt(0) ==
-              playerOneTappedColors.values.elementAt(3)) {
-        playerOneColorMatched = true;
+    if (player == 1) {
+      if (playerOneIsTapped[blockIndex] == false &&
+          playerOneTappedColors.length < 4) {
+        playerOneTappedColors[blockIndex] = colorsPlayerOne[blockIndex];
+        playerOneIsTapped[blockIndex] = true;
         notifyListeners();
-        playerOneColorsMatched();
-      } else {
-        playerOneTappedColors.clear();
-        playerOneIsTapped = List<bool>.filled(playerOneIsTapped.length, false);
+      }
+    } else {
+      if (playerTwoIsTapped[blockIndex] == false &&
+          playerTwoTappedColors.length < 4) {
+        playerTwoTappedColors[blockIndex] = colorsPlayerTwo[blockIndex];
+        playerTwoIsTapped[blockIndex] = true;
         notifyListeners();
       }
     }
   }
 
-  void updateStatesPlayerTwo() {
-    if (playerTwoTappedColors.length == 4) {
-      if (playerTwoTappedColors.values.elementAt(0) ==
-              playerTwoTappedColors.values.elementAt(1) &&
-          playerTwoTappedColors.values.elementAt(0) ==
-              playerTwoTappedColors.values.elementAt(2) &&
-          playerTwoTappedColors.values.elementAt(0) ==
-              playerTwoTappedColors.values.elementAt(3)) {
-        playerTwoColorMatched = true;
-        notifyListeners();
-        playerTwoColorsMatched();
-      } else {
-        playerTwoTappedColors.clear();
-        playerTwoIsTapped = List<bool>.filled(playerTwoIsTapped.length, false);
-        notifyListeners();
+  void updateStates(int player) {
+    if (player == 1) {
+      if (playerOneTappedColors.length == 4) {
+        if (playerOneTappedColors.values.elementAt(0) ==
+                playerOneTappedColors.values.elementAt(1) &&
+            playerOneTappedColors.values.elementAt(0) ==
+                playerOneTappedColors.values.elementAt(2) &&
+            playerOneTappedColors.values.elementAt(0) ==
+                playerOneTappedColors.values.elementAt(3)) {
+          playerOneColorMatched = true;
+          notifyListeners();
+          colorsMatched(player);
+        } else {
+          playerOneTappedColors.clear();
+          playerOneIsTapped =
+              List<bool>.filled(playerOneIsTapped.length, false);
+          notifyListeners();
+        }
+      }
+    } else {
+      if (playerTwoTappedColors.length == 4) {
+        if (playerTwoTappedColors.values.elementAt(0) ==
+                playerTwoTappedColors.values.elementAt(1) &&
+            playerTwoTappedColors.values.elementAt(0) ==
+                playerTwoTappedColors.values.elementAt(2) &&
+            playerTwoTappedColors.values.elementAt(0) ==
+                playerTwoTappedColors.values.elementAt(3)) {
+          playerTwoColorMatched = true;
+          notifyListeners();
+          colorsMatched(player);
+        } else {
+          playerTwoTappedColors.clear();
+          playerTwoIsTapped =
+              List<bool>.filled(playerTwoIsTapped.length, false);
+          notifyListeners();
+        }
       }
     }
   }
 
-  void playerOneColorsMatched() {
-    colorsPlayerOne
-        .removeWhere((element) => element == playerOneTappedColors[0]);
-    colorsPlayerOne
-        .removeWhere((element) => element == playerOneTappedColors[1]);
-    colorsPlayerOne
-        .removeWhere((element) => element == playerOneTappedColors[2]);
-    colorsPlayerOne
-        .removeWhere((element) => element == playerOneTappedColors[3]);
+  void colorsMatched(int player) {
+    if (player == 1) {
+      colorsPlayerOne
+          .removeWhere((element) => element == playerOneTappedColors[0]);
+      colorsPlayerOne
+          .removeWhere((element) => element == playerOneTappedColors[1]);
+      colorsPlayerOne
+          .removeWhere((element) => element == playerOneTappedColors[2]);
+      colorsPlayerOne
+          .removeWhere((element) => element == playerOneTappedColors[3]);
 
-    playerOneTappedColors.clear();
-    playerOneIsTapped = List<bool>.filled(playerOneIsTapped.length, false);
-    if (colorsPlayerOne.isEmpty) {
-      playerOneWon = true;
+      playerOneTappedColors.clear();
+      playerOneIsTapped = List<bool>.filled(playerOneIsTapped.length, false);
+      if (colorsPlayerOne.isEmpty) {
+        playerOneWon = true;
+      }
+      notifyListeners();
+      Future.delayed(const Duration(seconds: 2), () {
+        playerOneColorMatched = false;
+        notifyListeners();
+      });
+    } else {
+      colorsPlayerTwo
+          .removeWhere((element) => element == playerTwoTappedColors[0]);
+      colorsPlayerTwo
+          .removeWhere((element) => element == playerTwoTappedColors[1]);
+      colorsPlayerTwo
+          .removeWhere((element) => element == playerTwoTappedColors[2]);
+      colorsPlayerTwo
+          .removeWhere((element) => element == playerTwoTappedColors[3]);
+
+      playerTwoTappedColors.clear();
+      playerTwoIsTapped = List<bool>.filled(playerTwoIsTapped.length, false);
+      if (colorsPlayerTwo.isEmpty) {
+        playerTwoWon = true;
+      }
+      notifyListeners();
+      Future.delayed(const Duration(seconds: 2), () {
+        playerTwoColorMatched = false;
+        notifyListeners();
+      });
     }
-    notifyListeners();
-    Future.delayed(
-        const Duration(seconds: 2), () => playerOneColorMatched = false);
-    notifyListeners();
-  }
-
-  void playerTwoColorsMatched() {
-    colorsPlayerTwo
-        .removeWhere((element) => element == playerTwoTappedColors[0]);
-    colorsPlayerTwo
-        .removeWhere((element) => element == playerTwoTappedColors[1]);
-    colorsPlayerTwo
-        .removeWhere((element) => element == playerTwoTappedColors[2]);
-    colorsPlayerTwo
-        .removeWhere((element) => element == playerTwoTappedColors[3]);
-
-    playerTwoTappedColors.clear();
-    playerTwoIsTapped = List<bool>.filled(playerTwoIsTapped.length, false);
-    if (colorsPlayerTwo.isEmpty) {
-      playerTwoWon = true;
-    }
-    notifyListeners();
-    Future.delayed(
-        const Duration(seconds: 2), () => playerTwoColorMatched = false);
-    notifyListeners();
   }
 }
--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:rendering_testing/gameLogic.dart';
 
 class GamePage extends StatelessWidget {
-   const GamePage({super.key});
+  const GamePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +14,26 @@ class GamePage extends StatelessWidget {
         create: (context) => GameLogic(),
         child: Scaffold(body: Consumer<GameLogic>(
           builder: (context, value, child) {
+            final playerOneWon =
+                context.select((GameLogic p) => p.playerOneWon);
+            final playerTwoWon =
+                context.select((GameLogic p) => p.playerTwoWon);
+            final playerOneColorMatch =
+                context.select((GameLogic p) => p.playerOneColorMatched);
+            final playerTwoColorMatch =
+                context.select((GameLogic p) => p.playerTwoColorMatched);
+
             return Stack(children: [
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   /// player one board
-
+playerTwoColorMatch == false
+                  ?
                   Align(
                     alignment: Alignment.topCenter,
                     child: SizedBox(
-                      height: _height/2,
+                      height: _height / 2,
                       child: GridView.builder(
                           shrinkWrap: true,
                           physics: const NeverScrollableScrollPhysics(),
@@ -46,19 +56,24 @@ class GamePage extends StatelessWidget {
                                 ),
                               )),
                     ),
-                  ),
+                  )
+: FlutterLogo(),
+
+
                   /// player two board
                   Align(
-                   alignment: Alignment.bottomCenter,
+                    alignment: Alignment.bottomCenter,
                     child: RotatedBox(
                       quarterTurns: 2,
                       child: SizedBox(
-                        height: _height/2,
+                        height: _height / 2,
                         child: GridView.builder(
                             shrinkWrap: true,
                             physics: const NeverScrollableScrollPhysics(),
-                            itemCount:
-                                context.watch<GameLogic>().colorsPlayerTwo.length,
+                            itemCount: context
+                                .watch<GameLogic>()
+                                .colorsPlayerTwo
+                                .length,
                             gridDelegate:
                                 const SliverGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: 4,
@@ -69,9 +84,10 @@ class GamePage extends StatelessWidget {
                             itemBuilder: (context, index) => GestureDetector(
                                   onTap: () => value.gameLoop(2, index),
                                   child: Card(
-                                    color: value.playerTwoIsTapped[index] == false
-                                        ? Colors.redAccent
-                                        : value.colorsPlayerTwo[index],
+                                    color:
+                                        value.playerTwoIsTapped[index] == false
+                                            ? Colors.redAccent
+                                            : value.colorsPlayerTwo[index],
                                   ),
                                 )),
                       ),
